91c299e2d6dba9ddfec66a3be1135aa606ae9b79
D4LGONA <laff3y2297@gmail.com>
2023-01-18 17:21:27 +0900

    bird egg

--- character/ChararcterManager.py
+++ character/ChararcterManager.py
@@ -1,16 +1,22 @@
 from pico2d import *
 
+frame = 0
+
 
 class CharacterManager:
     pass
 
 
 class Dog(CharacterManager):
+
     def __init__(self):
-        self.image = load_image("dog.png")
+        self.image = load_image("bird1.png")
 
     def draw(self):
-        self.image.draw(200, 200)
+        global frame
+        self.image.clip_draw(frame * 32, 0, 32, 32, 200, 200, 128, 128)
+        frame = (frame + 1) % 2
+        delay(0.5)
 
 
 class Cat(CharacterManager):
